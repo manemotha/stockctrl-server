6dc0dea1ce18d7993d3b81b3017b49a4d710e5b0
Mane Motha <mane.motha@proton.me>
2025-07-13 16:30:33 +0200

    update: simplify http_response function by removing error handling

--- utils/controllers.py
+++ utils/controllers.py
@@ -1,17 +1,13 @@
-from typing import Any
 from fastapi.responses import JSONResponse
 
 
-def http_response(message: str, status_code: int) -> JSONResponse | tuple[Any, int]:
+def http_response(message: str, status_code: int) -> JSONResponse:
     """
     Generate a JSON HTTP response with a custom message and status code.
 
     :param message: The message to include in the response body.
     :param status_code: The HTTP status code for the response.
 
-    :returns: A FastAPI JSONResponse object with the given message and status code. If an error occurs, returns a JSONResponse with the error message and a 500 status code.
+    :returns: A FastAPI JSONResponse object with the given message and status code.
     """
-    try:
-        return JSONResponse(content={"message": message}, status_code=status_code)
-    except TypeError as error:
-        return JSONResponse(content={"message": error}, status_code=500)
+    return JSONResponse(content={"message": message}, status_code=status_code)
