42b81f91d7a70d4ca6833c0cee031ccc6368a9c4
Mane Motha <mane.motha@proton.me>
2025-09-02 08:43:54 +0200

    update: set min_length and max_length for admin signin and signup username

--- models/admin.py
+++ models/admin.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field, EmailStr, constr
 
 
 class AdminSignupModel(BaseModel):
-    username: constr(to_lower=True)
+    username: constr(to_lower=True) = Field(..., min_length=8, max_length=32)
     email: EmailStr
     password: str = Field(..., min_length=8, max_length=128)
     name: str = Field(..., min_length=1, max_length=100)
@@ -15,7 +15,7 @@ class AdminSignupModel(BaseModel):
 
 
 class AdminSigninModel(BaseModel):
-    username: constr(to_lower=True)
+    username: constr(to_lower=True) = Field(..., min_length=8, max_length=32)
     password: str = Field(..., min_length=8, max_length=128)
 
     # Forbid any extra fields in the request
